c7661dd6cc06782ca138b52d993b285346f11944
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-05-31 17:06:59 +0300

    Add Task 06 Dom

---
+++ Task_06_Dom/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа. 456 -> 5 782 -> 8 918 -> 1
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+// int secondDigit = num/10%10;
+// Console.WriteLine("Вторая цифра =  " + secondDigit);
+int GetSecondDigit(int number)
+{
+    int secDigit = number/10%10;
+    return secDigit;
+}
+int digit_2 = GetSecondDigit(num);
+Console.WriteLine("Вторая цифра = " + digit_2);
